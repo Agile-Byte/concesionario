bbef9f9fa2033e17ff731fdd56520d503239c734
gondaviza <gondaviza@gmail.com>
2023-11-26 22:32:04 +0100

    Añadido código

---
+++ tests/test_comun.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Test Comun"""
+
+import unittest
+from unittest import mock
+import pandas as pd
+
+from tests.extras.utils import df_clientes_existente, df_clientes_no_existente
+from src.proyecto.comun import leer_de_bd, escribir_en_bd, escribir_en_almacen, borrar_de_bd, modificar_en_bd, \
+    comprobar_existencia, generar_oferta_v2
+
+
+def test_leer_de_bd(self):
+    pass
+
+
+def test_escribir_en_bd(self):
+    pass
+
+
+def test_escribir_en_almacen(self):
+    pass
+
+
+def test_borrar_de_bd(self):
+    pass
+
+
+def test_modificar_en_bd(self):
+    pass
+
+
+def test_comprobar_existencia(self):
+    pass
+
+
+def test_generar_oferta_v2(self):
+    pass
